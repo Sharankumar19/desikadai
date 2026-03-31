912c9d245e1f3381350a03f472b086857ebd995f
icnk0048-sharankumar <sharankarunakaran19@gmail.com>
2026-03-31 23:41:28 +0530

    config update

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,15 +1,19 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()],
-  base: "/",  
-  server: {
-    proxy: {
-      '/api': {
-        target: env.VITE_BACKEND_URL,
-        changeOrigin: true,
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return defineConfig({
+    plugins: [react()],
+    base: "/",
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_BACKEND_URL, 
+          changeOrigin: true,
+        }
       }
     }
-  }
-})
+  })
+}
