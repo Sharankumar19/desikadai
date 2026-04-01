c1d876ca118f255991c930463d314b45848ce2d1
icnk0048-sharankumar <sharankarunakaran19@gmail.com>
2026-04-01 12:14:38 +0530

    change code to production

--- server/db.js
+++ server/db.js
@@ -1,16 +1,34 @@
+// local
+// require('dotenv').config();
+// const { Sequelize } = require('sequelize');
+
+// const sequelize = new Sequelize(
+//   process.env.DB_NAME,
+//   process.env.DB_USER,
+//   process.env.DB_PASSWORD,
+//   {
+//     host: process.env.DB_HOST,
+//     port: process.env.DB_PORT,
+//     dialect: 'mysql',
+//     logging: false,
+//   }
+// );
+
+// module.exports = sequelize;
+
+
+// production
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: 'mysql',
-    logging: false,
-  }
-);
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'mysql',
+  dialectOptions: {
+    ssl: {
+      rejectUnauthorized: true, // needed for Railway/MySQL cloud
+    },
+  },
+  logging: false,
+});
 
 module.exports = sequelize;
