55e844f83e9eec86e5518b15629be13538a9e72d
icnk0048-sharankumar <sharankarunakaran19@gmail.com>
2026-06-10 20:23:26 +0530

    prevented website for hacker

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useEffect } from 'react';
 import { CartProvider } from './context/CartContext';
 import { CheckoutProvider } from './context/CheckoutContext';
 import Navbar from './components/Navbar';
@@ -12,29 +13,56 @@ import About from './pages/About';
 import Contact from './pages/Contact';
 import ProductDetails from './components/ProductDetails';
 
-const App = () => (
-  <BrowserRouter>
-    <CartProvider>
-      <CheckoutProvider>
-        <div className="min-h-screen flex flex-col">
-          <Navbar />
-          <main className="flex-1">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/cart" element={<Cart />} />
-              <Route path="/checkout" element={<Checkout />} />
-              <Route path="/payment" element={<Payment />} />
-              <Route path="/blog" element={<Blog />} />
-              <Route path="/about" element={<About />} />
-              <Route path="/contact" element={<Contact />} />
-              <Route path="/product/:id" element={<ProductDetails />} />
-            </Routes>
-          </main>
-          <Footer />
-        </div>
-      </CheckoutProvider>
-    </CartProvider>
-  </BrowserRouter>
-);
+function App() {
+  useEffect(() => {
+    const handleKeyDown = (e) => {
+      if (
+        e.key === "F12" ||
+        (e.ctrlKey && e.shiftKey && e.key === "I") ||
+        (e.ctrlKey && e.shiftKey && e.key === "J") ||
+        (e.ctrlKey && e.key === "U")
+      ) {
+        e.preventDefault();
+      }
+    };
+
+    const disableRightClick = (e) => {
+      e.preventDefault();
+    };
+
+    document.addEventListener("keydown", handleKeyDown);
+    document.addEventListener("contextmenu", disableRightClick);
+
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+      document.removeEventListener("contextmenu", disableRightClick);
+    };
+  }, []);
+
+  return (
+    <BrowserRouter>
+      <CartProvider>
+        <CheckoutProvider>
+          <div className="min-h-screen flex flex-col">
+            <Navbar />
+            <main className="flex-1">
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/cart" element={<Cart />} />
+                <Route path="/checkout" element={<Checkout />} />
+                <Route path="/payment" element={<Payment />} />
+                <Route path="/blog" element={<Blog />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/contact" element={<Contact />} />
+                <Route path="/product/:id" element={<ProductDetails />} />
+              </Routes>
+            </main>
+            <Footer />
+          </div>
+        </CheckoutProvider>
+      </CartProvider>
+    </BrowserRouter>
+  );
+}
 
 export default App;
